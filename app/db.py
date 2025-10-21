40b09eaaf29e84d4504f98c75318a7782cd50f94
Ainas Rahman <ainaskodavandy@example.com>
2025-10-21 21:42:50 +0530

    new commit

--- app/db.py
+++ app/db.py
@@ -1,14 +1,19 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os 
 from dotenv import load_dotenv
-
+import certifi
 
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB")
 
 
 # connect to mongo db uri
-client = AsyncIOMotorClient(MONGO_URI)
+client = AsyncIOMotorClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
+
 # communicate to databas
 db = client[MONGO_DB]
 
